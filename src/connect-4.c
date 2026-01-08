5be44fa7504de94b626366d3d31a6804134f8990
AndrewContegiacomo <andreacontegiacomo@gmail.com>
2026-01-08 15:45:05 +0100

    checkWin function & clear comments added

--- src/connect-4.c
+++ src/connect-4.c
@@ -1,5 +1,6 @@
 #include "connect-4.h"
 
+//Initialize board with all EMPTY spaces
 void initBoard(int board[ROWS][COLS]){
 
     for(int i = 0; i < ROWS; i++){
@@ -10,6 +11,9 @@ void initBoard(int board[ROWS][COLS]){
     printf("Board is ready to play!\n");
 }
 
+//Print Board with coordinates on top 
+//P1 = x
+//P2 = o
 void printBoard(int board[ROWS][COLS]) {
     
     printf(" 0 1 2 3 4 5 6\n"); 
@@ -32,12 +36,15 @@ void printBoard(int board[ROWS][COLS]) {
     printf("---------------\n");
 }
 
+//Insert Token in the Board
 int insertToken(int board[ROWS][COLS], int column, int player){
 
+    //Check validity of the column in input
     if (column < 0 || column >= COLS) {
         return -1;
     }
-
+    
+    //Starting check from last row of the column 
     for(int i = ROWS - 1; i != -1; i--){
         if(board[i][column] == EMPTY){
             board[i][column] = player;
@@ -45,6 +52,58 @@ int insertToken(int board[ROWS][COLS], int column, int player){
         }
     }
     return -1;
+}
+
+//Helper function to count n token of the same player in any direction
+//dr & dc used to indicate where we have to move vertically and horizontally
+//r & c indicate the input token in the turn that we check
+int countDirection(int board[ROWS][COLS], int r, int c, int dr, int dc, int player) {
+
+    int count = 1;
+    int startRow = r;
+    int startColumn = c;
+    r +=  dr;
+    c += dc;
+
+    //Firstly check limit cases in while
+    while(r >= 0 && r < ROWS && c >= 0 && c < COLS && board[r][c] == player){
+        count++;
+        r += dr;
+        c += dc;
+    }
+
+    //Change direction with * -1  
+    dr = dr * -1;
+    dc = dc * -1;
+    r = startRow + dr;
+    c = startColumn + dc;
+
+    while(r >= 0 && r < ROWS && c >= 0 && c < COLS && board[r][c] == player){
+        count++;
+        r += dr;
+        c += dc;
+    }
+        if(count >= 4) return 1;
+
+    return 0;
+}
+
+int checkWin(int board[ROWS][COLS], int r, int c, int player) {
+
+    // Check Horizontally (-)
+    if (countDirection(board, r, c, 0, 1, player)) return 1;
+
+    // Check Vertically (|)
+    if (countDirection(board, r, c, 1, 0, player)) return 1;
+
+    // Check Diagonal (\)
+    if (countDirection(board, r, c, 1, 1, player)) return 1;
+
+    // Check Diagonal (/)
+    if (countDirection(board, r, c, 1, -1, player)) return 1;
+
+    // No win found
+    return 0;
 }
 
 
